8ec174c1041515f1243645f8490db9c1ad5dcf26
Anton Stetsenko <anton.stetsenko@dataart.com>
2023-11-27 18:13:53 +0100

    delete dist

--- dist/assets/RepeatPage-aca922ac.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,r as o,o as a,c as n,a as r,w as c,b as s,d as _}from"./index-26a28df9.js";const l={},d=s("h1",null,"Не переймася, наступного разу вийде краще!",-1);function i(u,f){const e=o("router-link");return a(),n("main",null,[d,r(e,{to:"/"},{default:c(()=>[_("Повторити")]),_:1})])}const p=t(l,[["render",i]]);export{p as default};
