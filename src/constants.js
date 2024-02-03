8b142ac5e45b3b5b34b853fc7e47032cb6082320
fmazur <mazurfedor@gmail.com>
2024-02-03 14:52:44 +0100

    img updated

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,18 @@
 export const MATCH_QUANTITY = 5;
 export const MATCH_LIST = [
-  {image: 'mittens-blue.jpg', description: 'РУКАВИЧКИ'},
-  {image: 'dress-green.jpg', description: 'СУКНЯ'},
-  {image: 'socks-red.jpg', description: 'ШКАРПЕТКИ'},
-  {image: 'hat-yellow.jpg', description: 'ШАПКА'},
-  {image: 'tshirt-black.jpg', description: 'ФУТБОЛКА'},
-  {image: 'jeans-blue.jpg', description: 'ДЖИНСИ'},
-  {image:'jacket-doublecolor.jpg', description:'КУРТКА'},
-  {image:'underwear-blue.jpg', description:'ТРУСИ'}
+  // {image: 'mittens-blue.jpg', description: 'РУКАВИЧКИ'},
+  // {image: 'dress-green.jpg', description: 'СУКНЯ'},
+  // {image: 'socks-red.jpg', description: 'ШКАРПЕТКИ'},
+  // {image: 'hat-yellow.jpg', description: 'ШАПКА'},
+  // {image: 'tshirt-black.jpg', description: 'ФУТБОЛКА'},
+  // {image: 'jeans-blue.jpg', description: 'ДЖИНСИ'},
+  // {image:'jacket-doublecolor.jpg', description:'КУРТКА'},
+  // {image:'underwear-blue.jpg', description:'ТРУСИ'},
+  {image:'among.jpg', description:'СЕРЕД'},
+  {image:'behind.jpg', description:'ПОЗА'},
+  {image:'between.jpg', description:'ПРОМІЖ'},
+  {image:'in_front.jpg', description:'ПЕРЕД'},
+  {image:'inside.jpg', description:'ВСЕРЕДИНІ'},
+  {image:'on.jpg', description:'НА'},
+  {image:'under.jpg', description:'ПІД'},
 ]
